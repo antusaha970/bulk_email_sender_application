4b707a1ac8dde4740d3cdbabcd7f31d2254d250b
Antu Saha <antusaha970@gmail.com>
2025-01-07 13:44:01 +0600

    added serializer for our models

---
+++ mail/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import *
+
+
+class SMTPConfigurationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SMTPConfiguration
+        fields = "__all__"
+
+
+class EmailComposeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Email_Compose
+        fields = "__all__"
+
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = "__all__"
+
+
+class RecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipient
+        fields = "__all__"
+
+
+class OutboxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Outbox
+        fields = "__all__"
