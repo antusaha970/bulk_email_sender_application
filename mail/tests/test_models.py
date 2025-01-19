04f3f46cceb0ea1e0c54b35567620184946350a1
Antu Saha <antusaha970@gmail.com>
2025-01-19 16:20:21 +0600

    refactored test files as moduler

--- mail/tests/test_models.py
+++ mail/tests/test_models.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from faker import Faker
 from django.contrib.auth.models import User
-from .models import SMTPConfiguration, Email_Compose, Recipient, Outbox
-from .serializers import SMTPConfigurationSerializer, EmailComposeSerializer
+from ..models import SMTPConfiguration, Email_Compose, Recipient, Outbox
 
 
 class ModelTest(TestCase):
@@ -91,77 +90,3 @@ class ModelTest(TestCase):
         self.assertEqual(email_address, recipient.email_address)
         self.assertEqual(status, recipient.status)
         self.assertEqual(email_compose.id, recipient.email_compose.id)
-
-
-class SerializerTest(TestCase):
-    def setUp(self):
-        self.faker = Faker()
-        self.user = User(username=self.faker.first_name)
-        self.user.set_password(self.faker.password(length=10))
-        self.user.save()
-
-    def test_SMTPConfigurationSerializer_with_valid_data(self):
-        """
-            Test for the SMTP configuration serializer with valid data
-        """
-        username = self.faker.user_name()
-        password = self.faker.password(length=8)
-        data = {
-            'username': username,
-            'password': password
-        }
-        serializer = SMTPConfigurationSerializer(
-            data=data, context={'user': self.user})
-
-        self.assertEqual(True, serializer.is_valid())
-
-    def test_SMTPConfigurationSerializer_with_duplicate_username(self):
-        """
-            Test for the SMTP configuration serializer with invalid data like(Duplicate Username)
-        """
-        username = self.faker.user_name()
-        password = self.faker.password(length=8)
-        SMTPConfiguration.objects.create(
-            username=username, password=password, user=self.user)
-        data = {
-            'username': username,
-            'password': password
-        }
-        serializer = SMTPConfigurationSerializer(
-            data=data, context={'user': self.user})
-
-        self.assertEqual(False, serializer.is_valid())
-
-    def test_EmailComposeSerializer_with_valid_data(self):
-        username = self.faker.user_name()
-        password = self.faker.password(length=8)
-        config = SMTPConfiguration.objects.create(
-            username=username, password=password, user=self.user)
-
-        subject = self.faker.text(max_nb_chars=250)
-        body = " ".join(self.faker.texts(nb_texts=5))
-
-        _data = {
-            'subject': subject,
-            'body': body,
-            'configurations': config.id
-        }
-
-        serializer = EmailComposeSerializer(data=_data)
-
-        self.assertEqual(True, serializer.is_valid())
-
-    def test_EmailComposeSerializer_with_invalid_data(self):
-
-        subject = self.faker.text(max_nb_chars=250)
-        body = " ".join(self.faker.texts(nb_texts=5))
-
-        _data = {
-            'subject': subject,
-            'body': body,
-            'configurations': 10
-        }
-
-        serializer = EmailComposeSerializer(data=_data)
-
-        self.assertEqual(False, serializer.is_valid())
---
+++ mail/tests/test_serializers.py
@@ -0,0 +1,79 @@
+from django.test import TestCase
+from faker import Faker
+from django.contrib.auth.models import User
+from ..models import SMTPConfiguration
+from ..serializers import SMTPConfigurationSerializer, EmailComposeSerializer
+
+
+class SerializerTest(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.user = User(username=self.faker.first_name)
+        self.user.set_password(self.faker.password(length=10))
+        self.user.save()
+
+    def test_SMTPConfigurationSerializer_with_valid_data(self):
+        """
+            Test for the SMTP configuration serializer with valid data
+        """
+        username = self.faker.user_name()
+        password = self.faker.password(length=8)
+        data = {
+            'username': username,
+            'password': password
+        }
+        serializer = SMTPConfigurationSerializer(
+            data=data, context={'user': self.user})
+
+        self.assertEqual(True, serializer.is_valid())
+
+    def test_SMTPConfigurationSerializer_with_duplicate_username(self):
+        """
+            Test for the SMTP configuration serializer with invalid data like(Duplicate Username)
+        """
+        username = self.faker.user_name()
+        password = self.faker.password(length=8)
+        SMTPConfiguration.objects.create(
+            username=username, password=password, user=self.user)
+        data = {
+            'username': username,
+            'password': password
+        }
+        serializer = SMTPConfigurationSerializer(
+            data=data, context={'user': self.user})
+
+        self.assertEqual(False, serializer.is_valid())
+
+    def test_EmailComposeSerializer_with_valid_data(self):
+        username = self.faker.user_name()
+        password = self.faker.password(length=8)
+        config = SMTPConfiguration.objects.create(
+            username=username, password=password, user=self.user)
+
+        subject = self.faker.text(max_nb_chars=250)
+        body = " ".join(self.faker.texts(nb_texts=5))
+
+        _data = {
+            'subject': subject,
+            'body': body,
+            'configurations': config.id
+        }
+
+        serializer = EmailComposeSerializer(data=_data)
+
+        self.assertEqual(True, serializer.is_valid())
+
+    def test_EmailComposeSerializer_with_invalid_data(self):
+
+        subject = self.faker.text(max_nb_chars=250)
+        body = " ".join(self.faker.texts(nb_texts=5))
+
+        _data = {
+            'subject': subject,
+            'body': body,
+            'configurations': 10
+        }
+
+        serializer = EmailComposeSerializer(data=_data)
+
+        self.assertEqual(False, serializer.is_valid())
